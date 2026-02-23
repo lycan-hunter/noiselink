86a3600ddf3b26149c4cd9714922320b716035ce
Lyvelovt <lyvelovt@gmail.com>
2026-02-23 19:55:23 +0300

    Added tomlplusplus as sb

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <CLI/CLI.hpp>
 #include <iostream>
+#include <memory>
+#include <rtc/rtc.hpp>
+#include <string>
+#include <vector>
 
-int main(int argc, char** argv) {}
+int main(int argc, char** argv) { return 0; }
